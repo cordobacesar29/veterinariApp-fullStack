f78ad915ea86a62c3ed79382d91d1fa9eb3648f7
Cesar Cordoba <cordobacesar29@gmail.com>
2021-02-21 20:31:41 -0300

    cambio el puerto del servidor 5k a 6k

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,6 @@ const http = require('http');
 const requestHandler = require('./request-handler');
 const server = http.createServer(requestHandler);
 
-server.listen(5000, () => {
+server.listen(6000, () => {
     console.log('el servidor está funcionando');
 });
