aa03cd7c81372d13f763420e095b1a7f11ae80c9
Cesar Cordoba <cordobacesar29@gmail.com>
2021-02-23 16:18:27 -0300

    mostrar mensaje cuando la lista mascota está vacía

--- frontend/js/mascotas.js
+++ frontend/js/mascotas.js
@@ -17,25 +17,35 @@ let mascotas = [];
         if(Array.isArray(mascotasDelServer)){
             mascotas = mascotasDelServer;
         }
-
-        const htmlMascotas = mascotas.map((mascota, index)=>`
-        <tr>
-            <th scope="row">${index}</th>
-            <td>${mascota.tipo}</td>
-            <td>${mascota.nombre}</td>
-            <td>${mascota.propietario}</td>
-            <td>
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-secondary editar"><i class="far fa-edit"></i></button>
-                <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
-            </div>
-            </td>
-        </tr>
-        `).join('');
-        listaMascota.innerHTML = htmlMascotas;
-        
-        Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
-        Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
+        if(mascotas.length) {
+            const htmlMascotas = mascotas.map((mascota, index)=>`
+            <tr>
+                <th scope="row">${index}</th>
+                <td>${mascota.tipo}</td>
+                <td>${mascota.nombre}</td>
+                <td>${mascota.propietario}</td>
+                <td>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-secondary editar"><i class="far fa-edit"></i></button>
+                    <button type="button" class="btn btn-danger eliminar"><i class="fas fa-trash-alt"></i></button>
+                </div>
+                </td>
+            </tr>
+            `).join('');
+            listaMascota.innerHTML = htmlMascotas;
+            Array
+                .from(document.getElementsByClassName('editar'))
+                .forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
+            Array
+                .from(document.getElementsByClassName('eliminar'))
+                .forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
+                return;
+        } else {
+            listaMascota.innerHTML =
+                `<tr>
+                    <td colspan="5">No hay mascotas</td>
+                </tr>`
+        }
 
     } catch (error) {
         console.log(error);
