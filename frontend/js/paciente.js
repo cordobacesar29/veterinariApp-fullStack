3548df07c83f28c4dc54b51c7ef747947e543cb9
Cesar Cordoba <cordobacesar29@gmail.com>
2021-02-25 16:50:11 -0300

    listar mascotas en un select en las consultas

--- frontend/js/paciente.js
+++ frontend/js/paciente.js
@@ -1,11 +1,14 @@
 const listaPacientes = document.getElementById('lista-pacientes');
-const url = 'http://localhost:5000/consultas';
+const mascotasPaciente = document.getElementById('mascota-paciente') ;
+const url = 'http://localhost:5000';
 
 let consultas = [];
+let mascotas = [];
 
 async function listarConsultas() {
+    const entidad = 'consultas';
     try {
-        const respuesta = await fetch(url);
+        const respuesta = await fetch(`${url}/${entidad}`);
         const consultasDelServidor =  await respuesta.json();
         if(Array.isArray(consultasDelServidor)) {
             consultas = consultasDelServidor;
@@ -30,19 +33,29 @@ async function listarConsultas() {
             listaPacientes.innerHTML = htmlConsultas;
         }
     } catch (error) {
-        
+        console.log(error);
     }
 }
 listarConsultas();
 
-
-/*
-    {
-        mascota: 0,
-        veterinaria:0,
-        fechaCreacion: new Date(),
-        fechaEdicion: new Date(),
-        historia: '',
-        diagnostico: ''
+async function listarMascotas() {
+    const entidad = 'mascotas';
+    try {
+        const respuesta = await fetch(`${url}/${entidad}`);
+        const mascotasDelServidor =  await respuesta.json();
+        if(Array.isArray(mascotasDelServidor)) {
+            mascotas = mascotasDelServidor;
+        }
+        if(respuesta.ok) {
+            mascotas.forEach((mascota, indice) => {
+                const optionActual = document.createElement('option');
+                optionActual.innerHTML = mascota.nombre;
+                optionActual.value = indice;
+                mascotasPaciente.appendChild(optionActual);
+            })
+        }
+    } catch (error) {
+        console.log(error);
     }
-*/
+}
+listarMascotas();
